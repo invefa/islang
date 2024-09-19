e0e38bbfd7b9dd3efdb84871fda7a8d0763edeba
invefa <invefa@outlook.com>
2024-09-19 13:52:22 +0800

    update xssert test code init ansictrl file

--- src/islang.c
+++ src/islang.c
@@ -13,10 +13,79 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "isl_xssert.h"
 #include "isl_overload.h"
+#include "isl_ansictrl.h"
 
+void isl_test_overload(void);
+void isl_test_xssert(void);
+
+
+int main(void) {
+
+    // typeof(0) num = 0;
+    // printf("Hello, World!\nnum = %d\n", num);
+
+    isl_test_overload();
+    isl_test_xssert();
+
+    return 0;
+}
+
+
+void isl_test_xssert(void) {
+
+#   define ISL_ASSERT_FAILURE_CODE 24605
+#   define __ISL_XSSERT_EXIT_a() exit(ISL_ASSERT_FAILURE_CODE);
+#   define __ISL_XSSERT_EXIT_w()
+
+#   define __ISL_XSSERT_SELECTOR_0()
+#   define __ISL_XSSERT_EXT_0()
+
+
+#   define __ISL_XSSERT_SELECTOR_1(_expl) \
+        "\tit can be explained as: " ANSI_HIL _expl ANSI_RST "\n"
+
+#   define __ISL_XSSERT_EXT_1(_expl)
+
+
+#   define __ISL_XSSERT_SELECTOR_2(_expl, _details) \
+        __ISL_XSSERT_SELECTOR_1(_expl)\
+        "\tthere are some details: " ANSI_HIL _details ANSI_RST "\n"
+
+#   define __ISL_XSSERT_EXT_2(_expl, _details)
+
+
+#   define __ISL_MACRO_UNPACKAGER(_package...) _package
+
+#   define __ISL_XSSERT_SELECTOR_3(_expl, _details_fmt, _fmt_args) \
+        __ISL_XSSERT_SELECTOR_2(_expl, _details_fmt)
+
+#   define __ISL_XSSERT_EXT_3(_expl, _details_fmt, _fmt_args) \
+        ,__ISL_MACRO_UNPACKAGER _fmt_args
+
+
+#   define __ISL_XSSERT(_x, _expr, _vargs...)                                       \
+        do{                                                                         \
+            if(!(_expr)) {                                                          \
+                fprintf(stderr, ANSI_HIR #_x "ssert failure:" ANSI_RST              \
+                "\n\tin file '"__FILE__"':\n\tat fn %s(...): expression: "          \
+                ANSI_HIL #_expr ANSI_RST " <line:%d>.\n"                            \
+                _isl_overload(__ISL_XSSERT_SELECTOR,##_vargs)                       \
+                , __func__, __LINE__ _isl_overload(__ISL_XSSERT_EXT,##_vargs));     \
+                __ISL_XSSERT_EXIT_##_x()                                            \
+            }\
+        } while(0)
+
+    int num = 1;
+    __ISL_XSSERT(w, num != 1);
+    __ISL_XSSERT(w, num != 1, "num should not be 1.");
+    __ISL_XSSERT(w, num != 1, "num should not be 1.", "you mutherfucker~");
+    __ISL_XSSERT(a, num != 1, "num should not be 1.", "num = %d, num + 1 = %d", (num, num + 1));
+
+}
 
 void test_overload_0(void) {
     printf("test_overload_0\n");
@@ -30,21 +99,15 @@ void test_overload_2(int _a, int _b) {
     printf("test_overload_2\narg_a = %d\narg_b = %d\n", _a, _b);
 }
 
-int main(void) {
+void isl_test_overload(void) {
 
-    typeof(0) a = __ISL_MACRO_GET_ARGS_COUNT(1, 2, 3, 4, 5);
-    printf("Hello, World!\na = %d\n", a);
-
-    __ISL_OVERLOAD_FORWARD_CALL(test_overload, a);
+    __ISL_OVERLOAD_FORWARD_CALL(test_overload, 1);
 
 #   define test_overload(_vargs...) _isl_overload(test_overload,##_vargs)
-
 
     test_overload();
     test_overload(123456);
     test_overload(123, 456);
-
-    return 0;
 
 }
 
--- src/utils/isl_xssert.h
+++ src/utils/isl_xssert.h
@@ -5,8 +5,8 @@
     which are used in debug mode to detect the error of the core code of islang.
 */
 
-
 #include "isl_options.h"
+#include "isl_ansictrl.h"
 
 #ifdef ISL_ENABLE_XSSERT
 
