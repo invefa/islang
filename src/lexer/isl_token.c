99181a404791acf7634fd9a89571056ba1fdbc0d
invefa <invefa@outlook.com>
2024-10-11 13:49:05 +0800

    optimize token: reconstruct location&token init methods to cons macro

--- src/lexer/isl_token.c
+++ src/lexer/isl_token.c
@@ -18,51 +18,51 @@ const ist_string ist_token_names[] = {
 #   undef manifest
 };
 
-inline void ist_location_init(ist_location* this, struct ist_codepage* _codepage) {
-    this->codepage = _codepage;
-    this->line = 1;
-    this->column = 1;
-}
+// inline void ist_location_initby_full(ist_location* this, struct ist_codepage* _codepage) {
+//     this->codepage = _codepage;
+//     this->line = 1;
+//     this->column = 1;
+// }
 
-/* nothing to clean now */
-void ist_location_clean(ist_location* this) {
-    isl_ifnreport(this, rid_catch_nullptr, isp_catch_coreloc);
-    // if (this->module) ist_string_clean(&this->module);
-}
+// /* nothing to clean now */
+// void ist_location_clean(ist_location* this) {
+//     isl_ifnreport(this, rid_catch_nullptr, isp_catch_coreloc);
+//     // if (this->module) ist_string_clean(&this->module);
+// }
 
-inline void ist_token_inito_null(ist_token* this) {
-    this->type = ISL_TOKENT_EOF;
 
-    ist_location_init(&this->location, NULL);
+// inline void ist_token_init_empty(ist_token* this) {
 
-    this->extract = NULL;
-    this->length = 0;
-    this->value.int_value = 0;
+//     ist_location_initby_full(&this->location, NULL);
+//     this->type = ISL_TOKENT_EOF;
+//     this->extract = NULL;
+//     this->length = 0;
+//     this->value.int_value = 0;
 
-}
+// }
 
-inline void ist_token_initby_location(ist_token* this, ist_location _location) {
-    this->type = ISL_TOKENT_EOF;
-    this->location = _location;
-    this->extract = NULL;
-    this->length = 0;
-    this->value.int_value = 0;
-}
+// inline void ist_token_initby_location(ist_token* this, ist_location _location) {
+//     this->type = ISL_TOKENT_EOF;
+//     this->location = _location;
+//     this->extract = NULL;
+//     this->length = 0;
+//     this->value.int_value = 0;
+// }
 
-inline void ist_token_initby_full(
-    ist_token* this,
-    ist_token_type  _type,
-    ist_location    _location,
-    ist_string      _extract,
-    ist_usize       _length,
-    ist_value       _value)
-{
-    this->type = _type;
-    this->location = _location;
-    this->extract = _extract;
-    this->length = _length;
-    this->value = _value;
-}
+// inline void ist_token_initby_full(
+//     ist_token* this,
+//     ist_token_type  _type,
+//     ist_location    _location,
+//     ist_string      _extract,
+//     ist_usize       _length,
+//     ist_value       _value)
+// {
+//     this->type = _type;
+//     this->location = _location;
+//     this->extract = _extract;
+//     this->length = _length;
+//     this->value = _value;
+// }
 
 inline ist_string* ist_token_dump(ist_token* this, ist_string* _buffer) {
     ist_byte storager = 0;
--- src/lexer/isl_token.h
+++ src/lexer/isl_token.h
@@ -23,8 +23,15 @@ typedef struct ist_location {
     ist_usize            column;
 } ist_location;
 
-void ist_location_init(ist_location* this, struct ist_codepage* _codepage);
-void ist_location_clean(ist_location* this);
+#define ist_location_consby_codepage(_codepage) \
+((ist_location){                \
+    .codepage = (_codepage),    \
+    .line     = 1,              \
+    .column   = 1               \
+})
+
+// void ist_location_initby_full(ist_location* this, struct ist_codepage* _codepage);
+// void ist_location_clean(ist_location* this);
 
 /*
     token is the basic unit for parser to analyze.
@@ -38,16 +45,38 @@ typedef struct ist_token {
     ist_value       value;
 } ist_token;
 
-void ist_token_inito_null(ist_token* this);
-void ist_token_initby_location(ist_token* this, ist_location _location);
+#define ist_token_cons_empty()      \
+((ist_token){                       \
+    .value.int_value = 0,           \
+    .type     = ISL_TOKENT_UNKNOWN, \
+    .extract  = NULL,               \
+    .length   = 0,                  \
+    .location = {                   \
+        .codepage = NULL,           \
+        .line     = 1,              \
+        .column   = 1               \
+    }                               \
+})
 
-void ist_token_initby_full(
-    ist_token* this,
-    ist_token_type  _type,
-    ist_location    _location,
-    ist_string      _extract,
-    ist_usize       _length,
-    ist_value       _value);
+#define ist_token_consby_full(_type, _location, _extract, _length, _value) \
+((ist_token){                 \
+    .type     = (_type),      \
+    .extract  = (_extract),   \
+    .length   = (_length),    \
+    .location = (_location),  \
+    .value    = (_value)      \
+})
+
+// void ist_token_init_empty(ist_token* this);
+// void ist_token_initby_location(ist_token* this, ist_location _location);
+
+// void ist_token_initby_full(
+//     ist_token* this,
+//     ist_token_type  _type,
+//     ist_location    _location,
+//     ist_string      _extract,
+//     ist_usize       _length,
+//     ist_value       _value);
 
 ist_string* ist_token_dump(ist_token* this, ist_string* _buffer);
 
