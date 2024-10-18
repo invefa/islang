bebb2e3bb9fd8f6c380d61076159e3616d8bfe77
invefa <invefa@outlook.com>
2024-10-18 21:19:24 +0800

    optimize report&repid: format with clang-format

--- src/report/isl_repids.h
+++ src/report/isl_repids.h
@@ -14,50 +14,86 @@ manifest(custom_core_error, (CORE, ERROR, CUSTOM), NULL)
 manifest(custom_core_panic, (CORE, PANIC, CUSTOM), NULL)
 manifest(custom_core_fatal, (CORE, FATAL, CUSTOM), NULL)
 
+manifest(utf8_negative_codepoint, (CORE, ERROR, CORELOC), "cannot encode negative codepoint.")
+manifest(catch_size_overflow, (CORE, FATAL, CORELOC), "catched an overflow size.")
+manifest(open_file_failed, (CORE, FATAL, CORELOC), "failed to open file '%s', does it exist?")
+manifest(illegal_utf8_codepoint, (CORE, FATAL, CORELOC), "cannot encode illegal UTF-8 codepoint.")
+manifest(unreachable_brench, (CORE, FATAL, CORELOC), "the program entered an unreachable branch...")
+manifest(
+    catch_nullptr,
+    (CORE, FATAL, CORELOC),
+    "catched a nullptr, the address should not be null."
+)
+manifest(
+    illegal_utf8_sequence,
+    (CORE, FATAL, CORELOC),
+    "cannot decode illegal UTF-8 sequence, did you get the index wrong?"
+)
+manifest(
+    incomplete_utf8_sequence,
+    (CORE, FATAL, CORELOC),
+    "cannot decode incomplete UTF-8 sequence, the buffer is not long enough."
+)
 
-manifest(unreachable_brench, (CORE, FATAL, CORELOC),
-"the program entered an unreachable branch...")
-manifest(catch_nullptr, (CORE, FATAL, CORELOC),
-"catched a nullptr, the address should not be null.")
-manifest(catch_size_overflow, (CORE, FATAL, CORELOC),
-"catched an overflow size.")
+manifest(
+    inform_allocated,
+    (CORE, INFO, NONE),
+    "allocate <0x%zX> {length=%zu}, then allocated-length=%zu."
+)
+manifest(
+    inform_released,
+    (CORE, INFO, NONE),
+    "released <0x%zX> {length=%zu}, then allocated-length=%zu."
+)
+manifest(
+    inform_buffer_reisze,
+    (CORE, INFO, NONE),
+    "buffer<0x%zX> {capacity=%zu, remaining=%zu} ->require(%zu) ->resize(%zu)."
+)
+manifest(
+    inform_list_reisze,
+    (CORE, INFO, NONE),
+    "list<0x%zX> {capacity=%zu, remaining=%zu} ->require(%zu) ->resize(%zu)."
+)
 
-manifest(open_file_failed, (CORE, FATAL, CORELOC),
-"failed to open file '%s', does it exist?")
+manifest(
+    catch_zero_string_length,
+    (CORE, WARNING, CORELOC),
+    "there is a zero-length string: <0x%zX>, please note its existence."
+)
+manifest(
+    catch_zero_capacity_list,
+    (CORE, WARNING, CORELOC),
+    "there is a zero-capacity list: <0x%zX>, please note its existence."
+)
+manifest(
+    catch_zero_capacity_buffer,
+    (CORE, WARNING, CORELOC),
+    "there is a zero-capacity buffer<0x%zX>, please note its existence."
+)
 
-manifest(utf8_negative_codepoint, (CORE, ERROR, CORELOC),
-"cannot encode negative codepoint.")
-manifest(illegal_utf8_codepoint, (CORE, FATAL, CORELOC),
-"cannot encode illegal UTF-8 codepoint.")
-manifest(illegal_utf8_sequence, (CORE, FATAL, CORELOC),
-"cannot decode illegal UTF-8 sequence, did you get the index wrong?")
-manifest(incomplete_utf8_sequence, (CORE, FATAL, CORELOC),
-"cannot decode incomplete UTF-8 sequence, the buffer is not long enough.")
-
-
-manifest(inform_allocated, (CORE, INFO, NONE),
-"allocate <0x%zX> {length=%zu}, then allocated-length=%zu.")
-manifest(inform_released, (CORE, INFO, NONE),
-"released <0x%zX> {length=%zu}, then allocated-length=%zu.")
-manifest(inform_buffer_reisze, (CORE, INFO, NONE),
-"buffer<0x%zX> {capacity=%zu, remaining=%zu} ->require(%zu) ->resize(%zu).")
-manifest(inform_list_reisze, (CORE, INFO, NONE),
-"list<0x%zX> {capacity=%zu, remaining=%zu} ->require(%zu) ->resize(%zu).")
-
-manifest(catch_zero_string_length, (CORE, WARNING, CORELOC),
-"there is a zero-length string: <0x%zX>, please note its existence.")
-manifest(catch_zero_capacity_list, (CORE, WARNING, CORELOC),
-"there is a zero-capacity list: <0x%zX>, please note its existence.")
-manifest(catch_zero_capacity_buffer, (CORE, WARNING, CORELOC),
-"there is a zero-capacity buffer<0x%zX>, please note its existence.")
-
-manifest(advance_codepoint_when_eof, (LEXER, WARNING, CORELOC),
-"advance codepoint when we reach the end of source.")
-manifest(unterminated_comment_block, (LEXER, WARNING, USERLOC),
-"block comments do not end with '*/', but with the end of file.")
-manifest(unterminated_string, (LEXER, WARNING, USERLOC),
-"the string do not end with '\"', but with the end of file.")
-manifest(unrecongnized_codepoint, (LEXER, WARNING, USERLOC),
-"unrecongnized unicode codepoint: <0x%X>, it will be skipped.")
-manifest(is_it_the_version_code, (LEXER, WARNING, USERLOC),
-"are you sure? is it the version code? the second dot and its suffix numbers will be ignored.")
+manifest(
+    advance_codepoint_when_eof,
+    (LEXER, WARNING, CORELOC),
+    "advance codepoint when we reach the end of source."
+)
+manifest(
+    unterminated_comment_block,
+    (LEXER, WARNING, USERLOC),
+    "block comments do not end with '*/', but with the end of file."
+)
+manifest(
+    unterminated_string,
+    (LEXER, WARNING, USERLOC),
+    "the string do not end with '\"', but with the end of file."
+)
+manifest(
+    unrecongnized_codepoint,
+    (LEXER, WARNING, USERLOC),
+    "unrecongnized unicode codepoint: <0x%X>, it will be skipped."
+)
+manifest(
+    is_it_the_version_code,
+    (LEXER, WARNING, USERLOC),
+    "are you sure? is it the version code? the second dot and its suffix numbers will be ignored."
+)
--- src/report/isl_report.h
+++ src/report/isl_report.h
@@ -5,16 +5,19 @@
     report system was the important part of islang.
     It is used to report some information to the user.
     And the report was divided into 5 levels:
-        1. info:    it was used to notify user some thing is going on, and supplied with some information.
-        2. warning: used to notify the user that something should not happen and needs to be fixed.
+        1. info:    it was used to notify user some thing is going on,
+                    and supplied with someinformation.
+        2. warning: used to notify the user that something should not
+                    happen and needs to be fixed.
         3. error:   notify user some thing can't be done.
         4. panic:   something is about to crash the program.
         5. fatal:   hell word, crash program.
     Written by invefa.
 */
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 
 #include "isl_string.h"
 
@@ -24,9 +27,20 @@
     So you can ignore it.
 */
 enum {
-    INFO, WARNING, ERROR, PANIC, FATAL,
-    CORE, LEXER, PARSER, COMPILER, VM,
-    NONE, CUSTOM, CORELOC, USERLOC,
+    INFO,
+    WARNING,
+    ERROR,
+    PANIC,
+    FATAL,
+    CORE,
+    LEXER,
+    PARSER,
+    COMPILER,
+    VM,
+    NONE,
+    CUSTOM,
+    CORELOC,
+    USERLOC,
 };
 
 
@@ -35,11 +49,11 @@ enum {
     It indicates the severity of the report.
 */
 typedef enum isp_level {
-    ISP_LEVEL_INFO = 0,
+    ISP_LEVEL_INFO    = 0,
     ISP_LEVEL_WARNING = 2,
-    ISP_LEVEL_ERROR = 4,
-    ISP_LEVEL_PANIC = 5,
-    ISP_LEVEL_FATAL = 6
+    ISP_LEVEL_ERROR   = 4,
+    ISP_LEVEL_PANIC   = 5,
+    ISP_LEVEL_FATAL   = 6
 } isp_level;
 
 /*
@@ -59,8 +73,9 @@ typedef enum isp_domain {
     Attribute for repid.
     NONE:       no attribute.
     CUSTOM:     indicate the fmt of repid will be provided by user.
-    CORELOC:    signed the core code exact location will be provided, it consumes 3 args. (core code location)
-    USERLOC:    signed the user code file location will be provided by user at the second arg of isl_report. (user file location)
+    CORELOC:    signed the core code exact location will be provided, it consumes 3 args. (core code
+   location) USERLOC:    signed the user code file location will be provided by user at the second
+   arg of isl_report. (user file location)
 */
 typedef enum isp_attribute {
     ISP_ATTR_NONE,
@@ -75,31 +90,27 @@ typedef enum isp_attribute {
     So I call it replocation, meaning the location of the repid.
 */
 typedef struct isp_replocation {
-    isp_level       level : 4;
-    isp_domain      domain : 4;
-    isp_attribute   attribute : 24;
+    isp_level     level    : 4;
+    isp_domain    domain   : 4;
+    isp_attribute attribute: 24;
 } isp_replocation;
 
 /* generate a replocation for repid */
-#define isp_gen_reploc(_domain, _level, _attr)  \
-    ((isp_replocation)                          \
-        {                                       \
-        .level=ISP_LEVEL_##_level,              \
-        .domain=ISP_DOMAIN_##_domain,           \
-        .attribute=ISP_ATTR_##_attr             \
-        })
+#define isp_gen_reploc(_domain, _level, _attr) \
+    ((isp_replocation                          \
+    ){.level = ISP_LEVEL_##_level, .domain = ISP_DOMAIN_##_domain, .attribute = ISP_ATTR_##_attr})
 
 
 /* there are some check table for repid */
-extern const ist_string         isp_fmts[];
-extern const ist_string         isp_repid_names[];
-extern const isp_replocation    isp_replocs[];
+extern const ist_string      isp_fmts[];
+extern const ist_string      isp_repid_names[];
+extern const isp_replocation isp_replocs[];
 
 /* the one who is served */
 typedef enum isp_repid {
-#   define manifest(_name, _reploc, _fmt) rid_##_name,
-#   include "isl_repids.h"
-#   undef manifest
+#define manifest(_name, _reploc, _fmt) rid_##_name,
+#include "isl_repids.h"
+#undef manifest
 } isp_repid;
 
 
@@ -108,16 +119,20 @@ void isl_report(isp_repid _rid, ...);
 
 
 /* if expr is false, then report */
-#define isl_ifnreport(_expr,_vargs...) \
-    do{if(!(_expr))isl_report(_vargs);}while(0)
+#define isl_ifnreport(_expr, _vargs...)   \
+    do {                                  \
+        if (!(_expr)) isl_report(_vargs); \
+    } while (0)
 
 
 /* if expr is true, then report */
-#define isl_ifreport(_expr,_vargs...) \
-    do{if(_expr)isl_report(_vargs);}while(0)
+#define isl_ifreport(_expr, _vargs...) \
+    do {                               \
+        if (_expr) isl_report(_vargs); \
+    } while (0)
 
 
 /* catch the core location for attribute:CORELOC of report */
-#define isp_catch_coreloc __FILE__,__func__,(ist_usize)__LINE__
+#define isp_catch_coreloc __FILE__, __func__, (ist_usize)__LINE__
 
 #endif
