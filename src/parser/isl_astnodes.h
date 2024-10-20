d00e4aeb5c23696360cf7d11cac72eeed1eb7814
invefa <invefa@outlook.com>
2024-10-20 11:40:08 +0800

    optimize astnodes: delete comment of codes

--- src/parser/isl_astnodes.h
+++ src/parser/isl_astnodes.h
@@ -55,31 +55,3 @@ manifest(FN_ENT, (IST_ASTNODE_REFERENCE_ENT))
 manifest(VAR_ENT, (IST_ASTNODE_REFERENCE_ENT))
 manifest(REGIST_ENT, (IST_ASTNODE_REFERENCE_ENT))
 manifest(TYPE_ENT, (IST_ASTNODE_REFERENCE_ENT))
-
-/*  unknown
-manifest(UNKNOWN)
-
- basic components
-manifest(NODE_LIST)
-manifest(ARG_LIST)
-manifest(PARAM_LIST)
-
- code structures
-manifest(MODULE)
-manifest(SCOPE)
-
- comptime entities
-manifest(REFEERENCE_ENT)
-manifest(NAME_ENT)
-manifest(LITERAL_ENT)
-manifest(FN_ENT)
-manifest(VAR_ENT)
-manifest(REGIST_ENT)
-manifest(TYPE_ENT)
-
- operations
-manifest(TERNARY_OPT)
-manifest(BINARY_OPT)
-manifest(LUNARY_OPT)
-manifest(RUNARY_OPT)
-*/
