196db9bbe34c4eeb7eec28f7f111906cf5805d17
invefa <invefa@outlook.com>
2024-10-20 10:57:50 +0800

    update astnode: comments

--- src/parser/isl_astnode.h
+++ src/parser/isl_astnode.h
@@ -18,6 +18,7 @@ enum {
 #undef manifest
 };
 
+/* define the type of the astnode */
 typedef ist_usize ist_astnode_type;
 enum ist_astnode_type {
 #define manifest(_name, _struct) ISL_ASTNT_##_name,
@@ -25,24 +26,24 @@ enum ist_astnode_type {
 #undef manifest
 };
 
+/* define the names of the astnode type */
 extern const ist_string ist_astnode_type_names[];
 
+
+/* define the base struct of the astnode */
 typedef struct ist_astnode {
     ist_astnode_type type;
     ist_location     location;
 } ist_astnode;
 
+
+/* define all the astnode struct */
 #define manifest(_name, _struct) typedef __ISL_MACRO_UNPACKAGER _struct IST_ASTNODE_##_name;
 #include "isl_astnodes.h"
 #undef manifest
 
-/**
- * Make the address as pointer of the specific type of astnode.
- */
-#define manifest(_name, _struct) IST_ASTNODE_##_name* ISL_AS_##_name(void* adr);
-#include "isl_astnodes.h"
-#undef manifest
 
+/* constructor for the astnode base */
 #define ist_astnode_consby_full(_type, _location) \
     ((ist_astnode){.type = (_type), .location = (_location)})
 
@@ -62,7 +63,18 @@ typedef struct ist_astnode {
         (void*)__result__;                                                                     \
     })
 
+/**
+ * Make the node as pointer of the specific type of astnode.
+ */
 #define ist_astnode_as(_node, _raw_type) ((IST_ASTNODE_##_raw_type*)_node)
+
+/**
+ * Make the address as pointer of the specific type of astnode.
+ * for all the astnode, you can use the fn to convert the address to the specific type.
+ */
+#define manifest(_name, _struct) IST_ASTNODE_##_name* ISL_AS_##_name(void* adr);
+#include "isl_astnodes.h"
+#undef manifest
 
 
 void ist_astnode_delete(void* this);
