6a41862368983c406b1723319d4d758767317479
invefa <invefa@outlook.com>
2024-11-03 20:22:30 +0800

    optimize some details

--- src/islang.c
+++ src/islang.c
@@ -60,10 +60,10 @@ int main(int argc, char* argv[]) {
     // isl_test_memgr();
     // isl_test_string();
     // isl_test_report();
-    // isl_test_lexer();
-    isl_test_generic();
+    isl_test_lexer();
+    // isl_test_generic();
     // isl_test_astnode();
-    // isl_test_parser();
+    isl_test_parser();
 
     return 0;
 }
