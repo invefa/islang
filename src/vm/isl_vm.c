0d0700bffbf23aefb3e5fe5ab8291b31fca6d11f
invefa <invefa@outlook.com>
2026-03-18 19:50:12 +0800

    Update vm: initial update, refine ist_vm struct, and add some constructor for it, add and init fn ist_vm_run.

--- src/vm/isl_vm.c
+++ src/vm/isl_vm.c
@@ -1,2 +1,53 @@
 #include "isl_vm.h"
 
+ist_vm ist_vm_consby_full(ist_instream instream) {
+    return (ist_vm){.instream = instream, .regist = {}, .stack = ist_value_stack_create(64, true)};
+}
+
+ist_vm* ist_vm_initby_full(ist_vm* this, ist_instream instream) {
+    *this = ist_vm_consby_full(instream);
+    return this;
+}
+ist_vm* ist_vm_createby_full(ist_instream instream) {
+    return ist_vm_initby_full(isl_allocate(sizeof(ist_vm), false), instream);
+}
+
+void ist_vm_run(ist_vm vm) {
+    ist_instruction* ip = vm.instream.data;
+    ist_value        ltmp, rtmp;
+    while (*ip != ist_inst_end) {
+        switch (*ip++) {
+            case ist_inst_add:
+                rtmp = ist_value_stack_pop(vm.stack);
+                ltmp = ist_value_stack_pop(vm.stack);
+                ist_value_stack_pushm(
+                    vm.stack, ist_value_consby_i64(ltmp.int_value + rtmp.int_value)
+                );
+                break;
+            case ist_inst_sub:
+                rtmp = ist_value_stack_pop(vm.stack);
+                ltmp = ist_value_stack_pop(vm.stack);
+                ist_value_stack_pushm(
+                    vm.stack, ist_value_consby_i64(ltmp.int_value - rtmp.int_value)
+                );
+                break;
+            case ist_inst_mul:
+                rtmp = ist_value_stack_pop(vm.stack);
+                ltmp = ist_value_stack_pop(vm.stack);
+                ist_value_stack_pushm(
+                    vm.stack, ist_value_consby_i64(ltmp.int_value * rtmp.int_value)
+                );
+                break;
+            case ist_inst_div:
+                rtmp = ist_value_stack_pop(vm.stack);
+                ltmp = ist_value_stack_pop(vm.stack);
+                ist_value_stack_pushm(
+                    vm.stack, ist_value_consby_i64(ltmp.int_value / rtmp.int_value)
+                );
+                break;
+
+            default:
+                isl_report(rid_unreachable_brench, isp_catch_coreloc);
+        }
+    }
+}
--- src/vm/isl_vm.h
+++ src/vm/isl_vm.h
@@ -1,11 +1,38 @@
 #ifndef ISC_VM_H
 #define ISC_VM_H
 
+#include "isl_instream.h"
 #include "isl_value.h"
 
-typedef struct ist_vm {
-    ist_value_stack* stack;
 
+typedef enum ist_vm_regist {
+    rax,
+    rbx,
+    rcx,
+    rdx,
+    rex,
+    rfx,
+    rgx,
+    rhx,
+    rix,
+    rsp,
+    rbp,
+    rip,
+    rss,
+    rcs,
+    rvs,
+    rsi,
+    rdi
+} ist_vm_regist;
+
+typedef struct ist_vm {
+    ist_instream     instream;
+    ist_value        regist[16];
+    ist_value_stack* stack;
 } ist_vm;
+
+ist_vm  ist_vm_consby_full(ist_instream instream);
+ist_vm* ist_vm_initby_full(ist_vm* this, ist_instream instream);
+ist_vm* ist_vm_createby_full(ist_instream instream);
 
 #endif // ISC_VM_H
