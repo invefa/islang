861e1d271a2a5a579ee7af541b8c4091f1154f18
invefa <invefa@outlook.com>
2026-03-18 19:45:00 +0800

    Init&Update isl_instream: initial update for instream.

---
+++ src/vm/isl_instream.c
@@ -0,0 +1,6 @@
+#include "isl_instream.h"
+
+#define ISG_VALUE_TYPE ist_instruction
+#define ISG_STRUCT_NAME ist_instream
+#define ISG_VALUE_DONT_CLEAN
+#include "isg_list_code.h"
---
+++ src/vm/isl_instream.h
@@ -0,0 +1,11 @@
+#ifndef ISC_INSTREAM_H
+#define ISC_INSTREAM_H
+#include "isl_instruction.h"
+
+#define ISG_VALUE_TYPE ist_instruction
+#define ISG_STRUCT_NAME ist_instream
+#include "isg_list_head.h"
+
+
+
+#endif // ISC_INSTREAM_H
