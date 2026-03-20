eb4236ff2d8201cbc5ade995a92ac52ebc0a7fd6
invefa <invefa@outlook.com>
2026-03-20 21:44:31 +0800

    Optimize some details

--- src/vm/isl_vm.c
+++ src/vm/isl_vm.c
@@ -40,7 +40,7 @@ void ist_vm_run(ist_vm vm) {
 #define ip_read_opcode(ipv)             (*(ipv)++)
 #define sp_pop(spv)                     (*(spv)--)
 #define sp_psh(spv, _value)             (*++(spv) = (_value))
-#define sp_pshas(spv, _as, _value)      ((++(spv))->as_##_as = (_value))
+#define sp_pshas(spv, _as, _asvalue)      ((++(spv))->as_##_as = (_asvalue))
 
 
     while (*vm.ip != ist_inst_end) {
