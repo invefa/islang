6fc6cf3d4a02888eb90a71f9227aa2c6b78a5530
invefa <invefa@outlook.com>
2026-04-15 22:31:12 +0800

    Update isl_compiler: Add elaboration and compilation fns.

--- src/compiler/isl_compiler.c
+++ src/compiler/isl_compiler.c
@@ -1,2 +1,43 @@
 #include "isl_compiler.h"
+#include "isl_memgr.h"
 
+void elaborate_expr(ist_compiler* this) {
+    switch (this->node->type) {
+        case isl_astnt_unary_expr:
+        case isl_astnt_binary_expr:
+        case isl_astnt_ternary_expr:
+            ist_context_register_compent(
+                this->ctx, ist_compent_createby_full(expr, this->node->location, res, ({
+                                                         res->is_const = false;
+                                                         res->node     = this->node;
+                                                     }))
+                // ist_compent_createm(
+                //     expr,
+
+                //     ((ist_compent_expr){
+                //         .base.kind     = isl_cpentkind_expr,
+                //         .base.location = this->node->location,
+                //         .is_const      = false,
+                //         .node          = this->node,
+                //     })
+                // )
+            );
+            break;
+        default:
+            isp_unreachable();
+    }
+}
+
+
+void ist_compiler_elaborate(ist_compiler* this) {
+    switch (this->node->type) {
+        case isl_astnt_binary_expr:
+
+            break;
+        default:
+            isp_unreachable();
+            break;
+    }
+}
+
+void ist_compiler_compile(ist_compiler* this) {}
--- src/compiler/isl_compiler.h
+++ src/compiler/isl_compiler.h
@@ -2,12 +2,17 @@
 #define ISC_COMPILER_H
 
 #include "isl_astnode.h"
+#include "isl_context.h"
 
 typedef struct ist_compiler {
+    ist_context* ctx;
+    ist_astnode* ast;
     ist_astnode* node;
-    
+
 } ist_compiler;
 
+void ist_compiler_elaborate(ist_compiler* this);
+void ist_compiler_compile(ist_compiler* this);
 
 
 #endif // ISC_COMPILER_H
