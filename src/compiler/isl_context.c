9d096057f7e4cd5315c2df616179cf1f3710fbba
invefa <invefa@outlook.com>
2026-05-09 17:46:05 +0800

    Update context.

--- src/compiler/isl_context.c
+++ src/compiler/isl_context.c
@@ -1,5 +1,22 @@
 #include "isl_context.h"
 
-inline ist_usize ist_context_register_compent(ist_context* this, ist_compent* _cpent) {
-    return ist_compentptr_list_addm(&this->compent_list, _cpent);
+ist_context ist_context_consby_void() {
+    return ist_context_{
+        .modules  = ist_moduleList_consm(1),
+        .compents = ist_compentList_consm(1),
+    };
+}
+_isl_define_initby_createby_with_consby((), (), ist_context, void);
+
+void ist_context_clean(ist_context* this) {
+    if (!this) return;
+    ist_moduleList_clean(&this->modules);
+    ist_compentList_clean(&this->compents);
+}
+_isl_define_delete_with_clean(ist_context);
+
+
+inline ist_compentRef ist_context_register_compent(ist_context* this, ist_compent cpent) {
+    //TODO: add a compare.
+    return ist_compentList_addm(&this->compents, cpent);
 }
--- src/compiler/isl_context.h
+++ src/compiler/isl_context.h
@@ -5,11 +5,23 @@
 #include "isl_module.h"
 
 typedef struct ist_context {
-    ist_module_list     module_list;
-    ist_compentptr_list compent_list;
+    ist_moduleList  modules;
+    ist_compentList compents;
 } ist_context;
 #define ist_context_ (ist_context)
 
-ist_usize ist_context_register_compent(ist_context* this, ist_compent* _cpent);
+ist_context ist_context_consby_void();
+_isl_declare_initby_createby_with_consby((), ist_context, void);
+
+void ist_context_clean(ist_context* this);
+_isl_declare_delete_with_clean(ist_context);
+
+ist_compentRef ist_context_acquire_compentRef(ist_context* this);
+ist_compent*   ist_context_acquire_compent(ist_context* this);
+ist_compentRef ist_context_register_compent(ist_context* this, ist_compent cpent);
+
+ist_moduleRef ist_context_acquire_moduletRef(ist_context* this);
+ist_module*   ist_context_acquire_module(ist_context* this);
+ist_moduleRef ist_context_register_module(ist_context* this, ist_module module);
 
 #endif // ISC_CONTEXT_H
